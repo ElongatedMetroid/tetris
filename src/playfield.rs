da7e0c4a5a551105f32bf90f1e2bc1142a1c621e
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-10-10 17:37:22 -0700

    re-implement playfield printing and add spawn_tetromino method

--- src/playfield.rs
+++ src/playfield.rs
@@ -1,15 +1,18 @@
 use core::fmt;
+use std::borrow::Borrow;
+
+use nalgebra::Vector2;
 
 use crate::{tetromino::Tetromino, cell::Cell};
 
 pub struct Playfield {
     // 10 cells wide by 24 cells tall 
-    cells: [[Cell; 10]; 24]
+    cells: Vec<Vec<Cell>>
 }
 
 impl fmt::Display for Playfield {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in self.cells {
+        for row in &self.cells {
             write!(f, "|").unwrap();
             for cell in row {
                 write!(f, "{}", cell).unwrap();
@@ -23,12 +26,32 @@ impl fmt::Display for Playfield {
 
 impl Playfield {
     pub fn new() -> Self {
+        let mut cells = Vec::new();
+
+        for _ in 0..24 {
+            let mut row = Vec::new();
+            for _ in 0..10 {
+                row.push(Cell::new(' ', Vector2::new(0, 0)));
+            }
+            cells.push(row);
+        }
+
         Self {
-            cells: [[' '; 10]; 24]
+            cells
         }
     }
     
-    pub fn spawn_tetrimino(&mut self, t: Tetromino) {
-        
+    pub fn spawn_tetromino(&mut self, t: Tetromino) {
+        let y = t.cell_data.main_cell.position.y as usize;
+        let x = t.cell_data.main_cell.position.x as usize;
+
+        self.cells[y][x] = t.cell_data.main_cell;
+
+        for cell in t.cell_data.attached_cells {
+            let y = (y as isize + cell.position.y) as usize;
+            let x = (x as isize + cell.position.x) as usize;
+
+            self.cells[y][x] = cell;
+        }
     }
 }
