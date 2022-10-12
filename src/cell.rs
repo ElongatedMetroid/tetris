4adf0e95a0e635000d27b88ef40eed85fb5648f8
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-10-12 16:27:41 -0700

    add display and tetromino part field

--- src/cell.rs
+++ src/cell.rs
@@ -1,23 +1,32 @@
-use std::rc::Rc;
+use std::fmt;
 
 use nalgebra::Vector2;
 
+#[derive(Clone)]
 pub struct Cell {
-    character: char,
-    position: Vector2<isize>,
+    pub(super) character: char,
+    /// Only used for spawning in the tetromino
+    pub(super) position: Vector2<isize>,
+    pub(super) tetromino_part: bool,
 }
 
 /// A collection of Cell's attached in a certain way
 pub struct CellBunch {
     /// Main cell of the bunch, this cell will be used to rotate around
-    main_cell: Cell,
-    /// Vector of positions in which attached cells will be at (thinking of the main_cell as position 0,0)
-    attached_cells: Vec<Rc<Cell>>
+    pub(super) main_cell: Cell,
+    /// Vector of Cell's (the position of the cells is relative to the main_cell)
+    pub(super) attached_cells: Vec<Cell>
 }
 
 pub struct CellBunchBuilder {
     main_cell: Cell,
-    attached_cells: Vec<Rc<Cell>>
+    attached_cells: Vec<Cell>
+}
+
+impl fmt::Display for Cell {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.character)
+    }
 }
 
 impl Cell {
@@ -25,12 +34,15 @@ impl Cell {
         Self {
             character,
             position,
+            tetromino_part: false,
         }
     }
 }
 
 impl CellBunch {
-    pub fn builder(main_cell: Cell) -> CellBunchBuilder {
+    pub fn builder(mut main_cell: Cell) -> CellBunchBuilder {
+        main_cell.tetromino_part = true;
+
         CellBunchBuilder {
             main_cell,
             attached_cells: Vec::new(),
@@ -43,8 +55,10 @@ impl CellBunchBuilder {
         CellBunch { main_cell: self.main_cell, attached_cells: self.attached_cells }
     }
 
-    pub fn attach_cell(mut self, cell: Cell) -> CellBunchBuilder {
-        self.attached_cells.push(Rc::new(cell));
+    pub fn attach_cell(mut self, mut cell: Cell) -> CellBunchBuilder {
+        cell.tetromino_part = true;
+
+        self.attached_cells.push(cell);
 
         self
     }
