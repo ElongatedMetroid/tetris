a89b0c0dc803f2753e83a000e77fc6fdb5142e6d
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-10-15 16:19:23 -0700

    add all pieces and all method

--- src/tetromino.rs
+++ src/tetromino.rs
@@ -42,14 +42,91 @@ impl Tetromino {
                         .attach_cell(Cell::new('■', Vector2::new(0, -2)))
                         .build()
                 }
-                TetrominoKind::J => todo!(),
-                TetrominoKind::L => todo!(),
-                TetrominoKind::O => todo!(),
-                TetrominoKind::S => todo!(),
-                TetrominoKind::T => todo!(),
-                TetrominoKind::Z => todo!(),
+                TetrominoKind::J => {
+                    // ■ <-- Main
+                    CellBunch::builder(Cell::new('▢', position))
+                        // ■ <-- Main
+                        // ■ <-- this
+                        .attach_cell(Cell::new('■', Vector2::new(0, 1)))
+                        // ■ <-- This
+                        // ■ <-- Main
+                        // ■
+                        .attach_cell(Cell::new('■', Vector2::new(0, -1)))
+                        //  ■
+                        //  ■ <-- Main
+                        // ■■
+                        .attach_cell(Cell::new('■', Vector2::new(-1, 1)))
+                        .build()
+                },
+                TetrominoKind::L => {
+                    // ■ <-- Main
+                    CellBunch::builder(Cell::new('▢', position))
+                        // ■ <-- Main
+                        // ■ <-- this
+                        .attach_cell(Cell::new('■', Vector2::new(0, 1)))
+                        // ■ <-- This
+                        // ■ <-- Main
+                        // ■
+                        .attach_cell(Cell::new('■', Vector2::new(0, -1)))
+                        // ■
+                        // ■ <-- Main
+                        // ■■
+                        .attach_cell(Cell::new('■', Vector2::new(1, 1)))
+                        .build()
+                },
+                TetrominoKind::O => {
+                    CellBunch::builder(Cell::new('▢', position))
+                        .attach_cell(Cell::new('■', Vector2::new(0, 1)))
+                        .attach_cell(Cell::new('■', Vector2::new(1, 0)))
+                        .attach_cell(Cell::new('■', Vector2::new(1, 1)))
+                        .build()
+                },
+                TetrominoKind::S => {
+                    CellBunch::builder(Cell::new('▢', position))
+                        .attach_cell(Cell::new('■', Vector2::new(1, 0)))
+                        .attach_cell(Cell::new('■', Vector2::new(0, 1)))
+                        .attach_cell(Cell::new('■', Vector2::new(-1, 1)))
+                        .build()
+                },
+                TetrominoKind::T => {
+                    CellBunch::builder(Cell::new('▢', position))
+                        .attach_cell(Cell::new('■', Vector2::new(1, 0)))
+                        .attach_cell(Cell::new('■', Vector2::new(0, 1)))
+                        .attach_cell(Cell::new('■', Vector2::new(-1, 0)))
+                        .build()
+                },
+                TetrominoKind::Z => {
+                    CellBunch::builder(Cell::new('▢', position))
+                        .attach_cell(Cell::new('■', Vector2::new(1, 0)))
+                        .attach_cell(Cell::new('■', Vector2::new(0, -1)))
+                        .attach_cell(Cell::new('■', Vector2::new(-1, -1)))
+                        .build()
+                },
             },
             rotation,
         }
+    }
+
+    /// Returns a vector containing all basic tetrominos
+    pub fn all(position: Vector2<isize>) -> Vec<Tetromino> {
+        let i = Tetromino::new(TetrominoKind::I, position, 0);
+        let j = Tetromino::new(TetrominoKind::J, position, 0);
+        let l = Tetromino::new(TetrominoKind::L, position, 0);
+        let o = Tetromino::new(TetrominoKind::O, position, 0);
+        let s = Tetromino::new(TetrominoKind::S, position, 0);
+        let t = Tetromino::new(TetrominoKind::T, position, 0);
+        let z = Tetromino::new(TetrominoKind::Z, position, 0);
+    
+        let mut pieces = Vec::new();
+
+        pieces.push(i);
+        pieces.push(j);
+        pieces.push(l);
+        pieces.push(o);
+        pieces.push(s);
+        pieces.push(t);
+        pieces.push(z);
+
+        pieces
     }
 }
