4b41f63b30a1fe5f9e384c8479bba4fc88733375
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-10-19 17:33:06 -0700

    add multithreading to handle movement and input

--- src/game.rs
+++ src/game.rs
@@ -1,4 +1,4 @@
-use std::{process, thread, time::Duration};
+use std::{process, thread, time::Duration, sync::{Arc, Mutex}};
 
 use device_query::{DeviceState, DeviceQuery, Keycode};
 use nalgebra::Vector2;
@@ -6,7 +6,7 @@ use rand::prelude::SliceRandom;
 
 use crate::{
     playfield::{Direction, Playfield},
-    tetromino::{Tetromino, TetrominoKind},
+    tetromino::Tetromino,
 };
 
 pub struct Game {
@@ -19,36 +19,58 @@ impl Game {
             playfield: Playfield::new(),
         }
     }
-    pub fn run(&mut self) {
+    pub fn run(self) {
         let pieces = Tetromino::all(Vector2::new(5, 10));
-        let mut rng = rand::thread_rng();
-        let device_state = DeviceState::new();
+        let mut piece = Arc::new(Mutex::new(pieces[0].clone()));
+        let game = Arc::new(Mutex::new(self));
 
-        loop {
-            let mut piece: Tetromino = pieces.choose(&mut rng).unwrap().clone();
-
-            self.playfield.spawn_tetromino(&mut piece);
+        let piece_clone = Arc::clone(&piece);
+        let game_clone = Arc::clone(&game);
+        // Keyboard input thread
+        thread::spawn(move || {
+            let device_state = DeviceState::new();
 
             loop {
-                print!("\x1B[2J\x1B[1;1H");
-                self.print_playfield();
-
-                thread::sleep(Duration::from_millis(500));
-
-                let direction = match device_state.get_keys().last() {
+                let direction = match device_state.get_keys().first() {
                     Some(&Keycode::Left) => Some(Direction::Left),
                     Some(&Keycode::Right) => Some(Direction::Right),
+                    Some(&Keycode::Down) => Some(Direction::Down),
                     _ => None,
                 };
 
                 if let Some(direction) = direction {
-                    self.playfield.move_tetromino(direction, &mut piece);
+                    game_clone.lock().unwrap().playfield.move_tetromino(direction, &mut piece_clone.lock().unwrap());
                 }
 
-                if self.playfield.move_tetromino(Direction::Down, &mut piece) {
-                    break;
+                thread::sleep(Duration::from_millis(100));
+            }
+        });
+        let piece_clone = Arc::clone(&piece);
+        let game_clone = Arc::clone(&game);
+        // Falling tetromino thread
+        thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+
+            loop {
+                *piece_clone.lock().unwrap() = pieces.choose(&mut rng).unwrap().clone();
+                game_clone.lock().unwrap().playfield.spawn_tetromino(&mut piece.lock().unwrap());
+    
+                loop {
+                    thread::sleep(Duration::from_millis(500));
+    
+                    if game_clone.lock().unwrap().playfield.move_tetromino(Direction::Down, &mut piece_clone.lock().unwrap()) {
+                        // Tetromino is now grounded, request a new tetromino
+                        break;
+                    }
                 }
             }
+        });
+
+        loop {
+            print!("\x1B[2J\x1B[1;1H");
+            game.lock().unwrap().print_playfield();
+
+            thread::sleep(Duration::from_millis(100));
         }
     }
     pub fn print_playfield(&self) {
