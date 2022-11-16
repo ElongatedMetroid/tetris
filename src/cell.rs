81fa0b659143c46eab72f687740e3e901918259b
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-11-15 16:45:10 -0800

    add new method for Cell

--- src/cell.rs
+++ src/cell.rs
@@ -4,3 +4,12 @@ pub struct Cell {
     pub(crate) character: char,
     pub(crate) position: Vector2<usize>,
 }
+
+impl Cell {
+    pub fn new(character: char, position: Vector2<usize>) -> Self {
+        Cell {
+            character,
+            position,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod cell;
 pub mod game;
 pub mod grid;
 pub mod playfield;
+pub mod tetromino;
