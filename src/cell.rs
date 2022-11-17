b31a7b11df5f68aebc87485b709dba412618f12f
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-11-16 17:58:42 -0800

    can_move field

--- src/cell.rs
+++ src/cell.rs
@@ -1,13 +1,16 @@
 use nalgebra::Vector2;
 
 pub struct Cell {
+    /// Controls whether the cell can move or not (ie fall)
+    pub(crate) can_move: bool,
     pub(crate) character: char,
-    pub(crate) position: Vector2<usize>,
+    pub(crate) position: Vector2<isize>,
 }
 
 impl Cell {
-    pub fn new(character: char, position: Vector2<usize>) -> Self {
+    pub fn new(can_move: bool, character: char, position: Vector2<isize>) -> Self {
         Cell {
+            can_move,
             character,
             position,
         }
