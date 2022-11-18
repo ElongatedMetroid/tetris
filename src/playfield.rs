54ea9004b86fae849e1467513d081593553760e3
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-11-17 17:25:04 -0800

    remove update_positions method in place for the move_uncheck method for moving the tetrominos

--- src/playfield.rs
+++ src/playfield.rs
@@ -1,18 +1,14 @@
 use core::fmt;
 use std::{cell::RefCell, rc::Rc};
 
-use crate::{
-    cell::Cell,
-    grid::Grid,
-    tetromino::Tetromino,
-};
+use crate::{cell::Cell, grid::Grid, tetromino::Tetromino};
 
-const PLAYFIELD_WIDTH: usize = 10;
-const PLAYFIELD_HEIGHT: usize = 24;
+pub const PLAYFIELD_WIDTH: usize = 10;
+pub const PLAYFIELD_HEIGHT: usize = 24;
 
 pub struct Playfield {
     /// The grid will hold refrences to Cell's
-    grid: Grid<Option<Rc<RefCell<Cell>>>>,
+    pub(crate) grid: Grid<Option<Rc<RefCell<Cell>>>>,
 }
 
 impl fmt::Display for Playfield {
@@ -59,33 +55,10 @@ impl Playfield {
     /// Spawn a tetromino
     pub fn spawn(&mut self, tetromino: &Tetromino) {
         for tetromino_cell in &tetromino.cells {
-            self.grid.grid[tetromino_cell.borrow().position.y]
-                [tetromino_cell.borrow().position.x] = Some(Rc::clone(&tetromino_cell));
+            self.grid.grid[tetromino_cell.borrow().position.y as usize]
+                [tetromino_cell.borrow().position.x as usize] = Some(Rc::clone(&tetromino_cell));
         }
     }
 
-    /// Update the positions of Cell's on the grid, match there position field to the corresponding position on the grid (the positions will
-    /// only be changed if the can_move field is true)
-    pub fn update_positions(&mut self) {
-        for real_y in (0..PLAYFIELD_HEIGHT).rev() {
-            for real_x in 0..PLAYFIELD_WIDTH {
-                let cell = match &self.grid.grid[real_y][real_x] {
-                    Some(cell) => cell,
-                    None => continue,
-                };
-
-                if cell.borrow().can_move {
-                    let cell_y = cell.borrow().position.y;
-                    let cell_x = cell.borrow().position.x;
-
-                    if (real_y, real_x) != (cell_y, cell_x) {
-                        let cell = self.grid.grid[real_y][real_x].take();
-
-                        self.grid.grid[cell_y][cell_x] = cell;
-                    }
-                }
-            }
-        }
-    }
     // A tetris will
 }
