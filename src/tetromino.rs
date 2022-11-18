bf4ed15be164236a21d16bad9bc7b377591a6ba2
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-11-17 17:24:22 -0800

    Create move_check method which validates that the place you want to move to is valid

--- src/tetromino.rs
+++ src/tetromino.rs
@@ -2,7 +2,7 @@ use std::{cell::RefCell, rc::Rc};
 
 use nalgebra::Vector2;
 
-use crate::cell::Cell;
+use crate::{cell::Cell, playfield::{Playfield, PLAYFIELD_WIDTH, PLAYFIELD_HEIGHT}};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Axis {
@@ -207,8 +207,14 @@ impl Tetromino {
         pieces
     }
 
-    pub fn move_unchecked(&self, axis: Axis, amount: isize) {
+    /// Move the piece without checking if it will hit a, an area that is out of bounds, or b, another cell
+    pub fn move_unchecked(&self, playfield: &mut Playfield, axis: Axis, amount: isize) {
         for cell in &self.cells {
+            // Take the cell from the grid
+            let cell_option = playfield.grid.grid[cell.borrow().position.y as usize]
+                [cell.borrow().position.x as usize]
+                .take();
+
             match axis {
                 Axis::Horizontal => {
                     cell.borrow_mut().position.x += amount;
@@ -217,6 +223,51 @@ impl Tetromino {
                     cell.borrow_mut().position.y += amount;
                 }
             }
+
+            // Put the cell back in the grid at the updated position
+            playfield.grid.grid[cell.borrow().position.y as usize]
+                [cell.borrow().position.x as usize] = cell_option;
         }
+    }
+
+    /// Move the piece with checking if it will collide with either a, the bounds of the playfield, or b, another cell. If it does collide
+    /// the piece will not move there and this will return false (will return true on a successful movement)
+    pub fn move_checked(&self, playfield: &mut Playfield, axis: Axis, amount: isize) -> bool {
+        // Check if the piece can move to the given position, then call move_unchecked
+        // Check each of the positions that this piece will take up once moved make sure they are None
+        for cell in &self.cells {
+            let mut y = cell.borrow().position.y as isize;
+            let mut x = cell.borrow().position.x as isize;
+
+            match axis {
+                Axis::Horizontal => {
+                    x += amount;
+                }
+                Axis::Vertical => {
+                    y += amount;
+                }
+            }
+
+            // Check if we are trying to move out of bounds
+            if x >= PLAYFIELD_WIDTH as isize || x < 0 || y >= PLAYFIELD_HEIGHT as isize || y < 0 {
+                return false;
+            }
+
+            match &playfield.grid.grid[y as usize][x as usize] {
+                Some(new_cell) => {
+                    // Check if the cell where we are trying to move is part of our tetromino
+                    if !self.cells.contains(&new_cell) {
+                        return false;
+                    }
+                }
+                None => {
+                    self.move_unchecked(playfield, axis, amount);
+
+                    return true;
+                },
+            }
+        }
+
+        true
     }
 }
