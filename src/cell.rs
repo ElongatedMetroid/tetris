1ec810e51a73f7acd6d402b71f60748104e070cc
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-11-24 13:01:48 -0800

    remove can_move field (it was set but unused)

--- src/cell.rs
+++ src/cell.rs
@@ -1,17 +1,14 @@
 use nalgebra::Vector2;
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone)]
 pub struct Cell {
-    /// Controls whether the cell can move or not (ie fall)
-    pub(crate) can_move: bool,
     pub(crate) character: char,
     pub(crate) position: Vector2<isize>,
 }
 
 impl Cell {
-    pub fn new(can_move: bool, character: char, position: Vector2<isize>) -> Self {
+    pub fn new(character: char, position: Vector2<isize>) -> Self {
         Cell {
-            can_move,
             character,
             position,
         }
